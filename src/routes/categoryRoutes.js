2cdbca29a7b73b5fb2041a4e69694461f9488970
Mamoudou12 <bmamoudou463@gmail.com>
2024-09-26 21:29:40 +0000

    adding categoyRoutes file

---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  getAllCategories,
+  getCategoryById,
+  createCategory,
+  updateCategory,
+  deleteCategory,
+} from '../controllers/categoryController.js';
+
+const router = express.Router();
+
+router.get('/categories', getAllCategories);
+router.get('/categories/:id', getCategoryById);
+router.post('/categories', createCategory);
+router.put('/categories/:id', updateCategory);
+router.delete('/categories/:id', deleteCategory);
+
+export default router;
